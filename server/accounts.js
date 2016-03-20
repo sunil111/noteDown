bd19b6a3e79b4968a1b803183b9ab225c3a49c4a
$un!l <langhi.sun@gmail.com>
2016-03-20 15:01:21 +0530

    Update accounts.js

--- server/accounts.js
+++ server/accounts.js
@@ -6,7 +6,7 @@ Accounts.loginServiceConfiguration.remove({
 Accounts.loginServiceConfiguration.insert({
   service: "facebook",
   appId: "153360715055453",
-  secret: "4e7e2e7d83713753838fc4a02a29984a"
+  secret: ""
 });
 
 // first, remove configuration entry in case service is already configured
@@ -25,6 +25,6 @@ Accounts.loginServiceConfiguration.remove({
 });
 Accounts.loginServiceConfiguration.insert({
   service: "google", clientId:"799441956859msqd4lvbo40kf3u7pnh9rudh8ti19hpl.apps.googleusercontent.com",
-  secret: "61yoKcCFDfafZ9Z8nKX_VW61"
+  secret: ""
 });
 });
