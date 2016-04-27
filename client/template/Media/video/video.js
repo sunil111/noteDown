04b34eaad9ee10391d3fa71b44194113b31c6c5a
sunil111 <langhi.sun@gmail.com>
2016-04-27 11:52:20 +0530

    updated

--- client/template/Media/video/video.js
+++ client/template/Media/video/video.js
@@ -2,6 +2,13 @@ Template.video.created = function(){
 	this.filename = new ReactiveVar('');
 };
 
+Template.video.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('videos');
+	});
+});
+
 
 Meteor.startup(function() {
 
