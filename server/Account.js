f1ba34940c090dad6349ddb0154ad6cf77e89f78
Roshan2892 <http://github.com/Roshan2892>
2016-04-27 12:51:50 +0530

    Account file

--- server/Account.js
+++ server/Account.js
@@ -2,9 +2,9 @@ Accounts.onCreateUser(function(options, user) {
     user.profile = user.profile || {}; //If the google service exists 
     if ((service = user.services) !== undefined ? service.google : undefined) { 
         user.profile.emails = user.services.google.email;
-        user.profile.name = user.services.google.name; 
+        user.profile.name = user.services.google.name;
     }
-    if ((service = user.services) !== undefined ? service.facebook : undefined) { 
+    else if ((service = user.services) !== undefined ? service.facebook : undefined) { 
         user.profile.emails = user.services.facebook.email;
         user.profile.name = user.services.facebook.name; 
     }
