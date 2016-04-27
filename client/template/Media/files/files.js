d66d3bdf62e033253dced31e3dd8d904cd853829
sunil111 <langhi.sun@gmail.com>
2016-04-27 11:52:02 +0530

    updated

--- client/template/Media/files/files.js
+++ client/template/Media/files/files.js
@@ -2,6 +2,13 @@ Template.files.created = function () {
   this.filename = new ReactiveVar('');
 };
 
+Template.files.onCreated(function(){
+  var self= this;
+  this.autorun( function() {
+    self.subscribe('files');
+  });
+});
+
 // Can't call getHandler until startup so that Collections object is available
 Meteor.startup(function () {
 
