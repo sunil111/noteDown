a771d0a1b90c92504d8c8d554cb420f163f65ae0
sunil111 <langhi.sun@gmail.com>
2016-05-02 00:20:27 +0530

    integrated SM Note

--- client/template/SmNote/SmNote.js
+++ client/template/SmNote/SmNote.js
@@ -1,9 +1,14 @@
+Template.SmNote.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('posts');
+	});
+});
+
 Template.SmNote.events({
 	
-	'submit #addPost' : function () {
+	'submit #addPost' : function (event) {
 		event.preventDefault();
-	
-
 
 		var title = event.target.postTitle.value;
 		var message = event.target.postMessage.value;
@@ -11,16 +16,89 @@ Template.SmNote.events({
 		//console.log(postBody);
 		Meteor.call('addPost', title, message, postBody, function (error) {
 			if(!error){
-			Router.go('/shownotes');	
+			Router.go('/showNotes');	
+			//alert("No Error");
 			}
 		});
 	}
 });
-
 
 //every time the template rendered
 Template.SmNote.onRendered(function () {
 		$(document).ready(function() {
 		  $('#summernote').summernote();
 		});
+});
+
+Template.ShowNotes.helpers({
+	posts: function() {
+		return Posts.find({});
+	}
+});
+
+Template.ShowNotes.events({
+	'click #deletePost': function () {
+		Meteor.call('deletePost', this._id);
+	}
+});
+
+Template.ShowNotes.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('posts');
+	});
+});
+
+Template.SinglePost.helpers({
+	post: function() { 
+		var id = Session.get('postId');
+		var post=Posts.findOne({_id: id});
+		return post;
+	}
+});
+
+Template.SinglePost.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('posts');
+	});
+});
+
+
+Template.EditPosts.events({
+	'submit #editPost' : function (event) {
+		event.preventDefault();
+		var id = Session.get('postId');
+		var title = event.target.postTitle.value;
+		var message = event.target.postMessage.value;
+		var postBody = $('#summernote').summernote('code');
+		Meteor.call('editPost',id, title, message, postBody, function (error) {
+			if(!error){
+				console.log('Successfully');
+				Router.go('ShowNotes');
+			}
+		});
+	}
+});
+
+Template.EditPosts.onRendered(function () {
+	$(document).ready(function() {
+		  $('#summernote').summernote();
+		});
+});
+
+Template.EditPosts.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('posts');
+	});
+});
+
+Template.EditPosts.helpers({
+	edit : function(){
+		var id = Session.get('postId');
+		var edit=Posts.findOne({_id: id});
+		return edit;
+	}
+
 });
