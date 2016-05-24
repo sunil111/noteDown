d27ae9358aad809a0d48fae330f9abcc982616de
sunil111 <langhi.sun@gmail.com>
2016-05-24 22:57:37 +0530

    Updated

--- server/main.js
+++
@@ -1,7 +0,0 @@
-Meteor.startup(function() {
-  console.log("Images Uploads:", Collections.Images.find().count());
-  console.log("Files:", Collections.Files.find().count());
-  console.log("Videos:", Collections.Videos.find().count());
-  console.log("Audios:", Collections.Files.find().count());
-});
-
