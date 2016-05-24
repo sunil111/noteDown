0d8ac01258b796a694ca167a9ef86303b2b43533
sunil111 <langhi.sun@gmail.com>
2016-05-24 12:35:37 +0530

    Updated

---
+++ client/template/Search/Search.js
@@ -0,0 +1,13 @@
+Template.Search.helpers({
+	searchIndexes: () => [groupsIndex, postsIndex],
+  		groupIndex: () => groupsIndex,
+  		postIndex: () => postsIndex
+});
+
+Template.Search.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('groups');
+		self.subscribe('posts');
+	});
+});
