28c15193c9da72a8a1ef1bd5b1fecbf8152bb066
sunil111 <langhi.sun@gmail.com>
2016-05-24 12:19:09 +0530

    Updated

--- client/Account.js
+++ client/Account.js
@@ -1,15 +1,10 @@
 Accounts.ui.config({
-  requestPermissions: {
-    facebook: ['email','']
-  },
-  requestOfflineToken: {
-    google: true
-  },
-  passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
+	requestPermissions: {
+	facebook: ['email','']
+	},
+	requestOfflineToken: {
+	google: true
+	},
+	passwordSignupFields: 'USERNAME_AND_OPTIONAL_EMAIL'
 });
-
-
-
-//--------------------------------------------------------------------------
-
 
