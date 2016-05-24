baf2a4531d7af990ad63c55da1270df9c5b2710e
sunil111 <langhi.sun@gmail.com>
2016-05-24 23:44:00 +0530

    tags modify

--- client/template/Tags/tags.js
+++ client/template/Tags/tags.js
@@ -6,8 +6,7 @@ Template.Tags.events({
 	"submit .form": function(event,tag){		
 		event.preventDefault();
 		var tagsVar = event.target.tagsTxt.value;
-		/*$('<div class="btn-group" style="margin-top:20px;"><input type="button" class="btn-group " id="tag'+ uniqueNum + uniquevar +'" value="'+ tagsVar + '" style=" border-right:none; border-radius:0px; border: 3px solid #808080; height:34px" readonly disabled><button class="glyphicon glyphicon-remove btn btn-group btn-danger btn-xs" id="tag'+uniqueNum +'" style="border-radius:0px; height:34px;"></button>&nbsp;&nbsp;&nbsp;&nbsp;</input></div>').insertAfter("h3");*/
-		$('<div class="input-group" style="margin-top:20px;"><input type="button" class="btn-group " id="tag'+ uniqueNum + uniquevar +'" value="'+ tagsVar + '" style=" border-right:none; border-radius:0px; border: 3px solid #808080; height:34px" readonly disabled><span class="input-group-btn"><button class="glyphicon glyphicon-remove btn btn-group btn-danger btn-xs btn-secondary" id="tag'+uniqueNum +'" style="border-radius:0px; height:34px;"></button>&nbsp;&nbsp;&nbsp;&nbsp;</input></span></div>').insertAfter("h3");
+		$('<div class="btn-group" style="margin-top:20px;"><input type="button" class="btn-group " id="tag'+ uniqueNum + uniquevar +'" value="'+ tagsVar + '" style=" border-right:none; border-radius:0px; border: 3px solid #808080; height:34px" readonly disabled><button class="glyphicon glyphicon-remove btn btn-group btn-danger btn-xs" id="tag'+uniqueNum +'" style="border-radius:0px; height:33px;"></button>&nbsp;&nbsp;&nbsp;&nbsp;</input></div>').insertAfter("h3");
     	var c = document.getElementById('tag'+ uniqueNum + uniquevar +'').value;
     	arr.push(c);
     	Session.set('tag', arr);
@@ -22,11 +21,3 @@ Template.Tags.events({
         $('[id="'+ id +'"]').remove();
 	}
 });
-
-
-/*<div class="btn-group" style="margin-top:20px;">
-<input type="button" class="btn-group " id="tag'+ uniqueNum + uniquevar +'" value="'+ tagsVar + '" style=" border-right:none; border-radius:0px; border: 3px solid #808080; height:34px" readonly disabled>
-<button class="glyphicon glyphicon-remove btn btn-group btn-danger btn-xs" id="tag'+uniqueNum +'" style="border-radius:0px; height:34px;"></button>&nbsp;&nbsp;&nbsp;&nbsp;</input>
-</div>*/
-
-
