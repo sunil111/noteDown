6081252adeeceeef083610f1f019cb3f74ba5d5a
sunil111 <langhi.sun@gmail.com>
2016-05-24 12:23:51 +0530

    Updated

--- client/template/Header/Header.js
+++ client/template/Header/Header.js
@@ -1,31 +1,9 @@
-Template.Header.onCreated(function(){
-	var self= this;
-	this.autorun( function() {
-		self.subscribe('rss');
+/*$(document).ready(function(e){
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("href").replace("#","");
+		var concept = $(this).text();
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
 	});
-});
-
-Template.Header.helpers({
-	feed:function(){
-		var feed= Rss.find();
-		return feed;
-	},
-	feedCount:function(){
-		return Rss.find().count();
-	}
-});
-
-Template.Header.events({
-	'click #check': function(event){
-		var id= this._id;
-		var action= this.action;
-		var itemId= this.id;
-		console.log(itemId);
-		if(action=== "Post"){
-			Router.go('/posts/'+itemId);
-		}
-		else if(action=== "Group"){
-			Router.go('/group/'+itemId);
-		}
-	}
-});
+});*/
