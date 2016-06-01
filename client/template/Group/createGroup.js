8e22d02c97cfbf3e440f315107a9c765cf5f40fd
sunil111 <langhi.sun@gmail.com>
2016-06-01 22:04:35 +0530

    Update createGroup.js

--- client/template/Group/createGroup.js
+++ client/template/Group/createGroup.js
@@ -1,46 +1,43 @@
-Template.newGroup.onCreated(function(){
+Template.CreateGroup.onCreated(function(){
       var self= this;
       this.autorun( function() {
             self.subscribe('groups');
       });
 });
 
-Template.newGroup.helpers({
+Template.CreateGroup.helpers({
       group_name: function(){
             return Groups.find({},{ gname: 1, _id: 0});
       }
 });
 
-Template.newGroup.events({
-	"submit .form": function(event) {
-      	event.preventDefault();
-      	var privacy_flag;
+Template.CreateGroup.events({
+      "submit .form": function(event) {
+            event.preventDefault();
+            var privacy_flag;
             // Get value from form element
             var gtitle = event.target.Title.value;
             var gdesc = event.target.Description.value;
             var result = Groups.findOne({ gname: gtitle });
             if (result) {
-                  alert("Group name already exists");
+                  alert("You have already created a group by this name");
                   event.target.Title.value = "";
                   return false;
             }
             // Insert a task into the collection
             if(event.target.Privacy.checked){
-            	privacy_flag = "private";
+                  privacy_flag = "private";
             }
             else{
-            	privacy_flag = "public";
+                  privacy_flag = "public";
             }
-            if(confirm("Are the details correct ?")== true){
-                  Meteor.call("addGroup", gtitle, gdesc, privacy_flag, function(err, res){
-            		if(!err){//all good
-                              alert('Group created succesfully');
-                              var group = Groups.findOne({ gname: gtitle });
-                              var id= group._id;
-                              Router.go('/group/'+id);
-            		}
-            	});
-            }
+            Meteor.call("addGroup", gtitle, gdesc, privacy_flag, function(err, res){
+                  if(!err){//all good
+                        var group = Groups.findOne({ gname: gtitle });
+                        var id= group._id;
+                        Router.go('/group/'+id);
+      		}
+      	});
             // Clear form
             event.target.Title.value = "";
             event.target.Description.value = "";
