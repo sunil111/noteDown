42124a7431e5e11e425290236a787865efa52eed
sunil111 <langhi.sun@gmail.com>
2016-06-01 22:08:50 +0530

    Update Media

--- client/template/Media/video/video.js
+++ client/template/Media/video/video.js
@@ -15,20 +15,19 @@ Meteor.startup(function() {
 	Template.video.events({
 	'change input.videoFile' : FS.EventHandlers.insertFiles(Collections.Videos,{
 		metadata : function(fileobj){
-			var groupId = Session.get('groupId');
 			return {
-				owner:{
-              		id: Meteor.userId(),
-             		name: Meteor.user().profile.name
-            	},
-            	groupID: groupId,
-            	dropped: false
-			};
+	          	owner:{
+	            	id: Meteor.userId(),
+	            	name: Meteor.user().profile.name
+	          	},
+	          	dropped: false,
+          		privacy:"private"
+	        };
 		},
 
 		after : function (error,fileobj){
 			if(!error){
-				console.log("Inserted",fileobj.name());
+				alert('done');
 			}
 		}
 	}),
@@ -45,7 +44,59 @@ Meteor.startup(function() {
 
 Template.video.helpers({
   uploadedVideos: function() {
-    var groupId = Session.get('groupId');
-    return Collections.Videos.find({groupID: groupId});
+    return Collections.Videos.find({});
+  }
+});
+
+
+Template.video_group.created = function(){
+	this.filename = new ReactiveVar('');
+};
+
+Template.video_group.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('videos');
+	});
+});
+
+
+Meteor.startup(function() {
+
+	Template.video_group.events({
+	'change input.videoFile' : FS.EventHandlers.insertFiles(Collections.Videos,{
+		metadata : function(fileobj){
+			var groupId = Session.get('groupId');
+		    	return {
+					owner:{
+						id: Meteor.userId(),
+						name: Meteor.user().profile.name
+					},
+					groupID: groupId,
+					dropped: false,
+              		privacy:"public"
+		        };
+		},
+
+		after : function (error,fileobj){
+			if(!error){
+				alert('done');
+			}
+		}
+	}),
+	
+	'keyup .filename': function(){
+		var ins = Template.instance();
+		if(ins){
+			ins.filename.set($('.filename').val());
+			}
+		}
+	});
+});
+
+
+Template.video_group.helpers({
+  uploadedVideos: function() {
+    return Collections.Videos.find({});
   }
 });
