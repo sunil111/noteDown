10c4e0991e49d2c19e8b628c1eb699662ec0fedf
sunil111 <langhi.sun@gmail.com>
2016-06-01 22:08:12 +0530

    Update Media

--- client/template/Media/files/files.js
+++ client/template/Media/files/files.js
@@ -15,19 +15,18 @@ Meteor.startup(function () {
   Template.files.events({
     'change input.any': FS.EventHandlers.insertFiles(Collections.Files, {
       metadata: function (fileObj) {
-        var groupId = Session.get('groupId');
         return {
-          owner:{
-            id: Meteor.userId(),
-            name: Meteor.user().profile.name
-          },
-          groupID: groupId,
-          foo: "bar",
-          dropped: false
-        };
+              owner:{
+                id: Meteor.userId(),
+                name: Meteor.user().profile.name
+              },
+              dropped: false,
+              privacy:"private"
+            };
       },
       after: function (error, fileObj) {
         if (!error) {
+          alert('done');
         }
       }
     }),
@@ -45,7 +44,59 @@ Meteor.startup(function () {
 
 Template.files.helpers({
   uploadedFiles: function() {
-    var groupId = Session.get('groupId');
-    return Collections.Files.find({groupID: groupId});
+    return Collections.Files.find({});
+  }
+});
+
+
+Template.files_group.created = function () {
+  this.filename = new ReactiveVar('');
+};
+
+Template.files_group.onCreated(function(){
+  var self= this;
+  this.autorun( function() {
+    self.subscribe('files');
+  });
+});
+
+// Can't call getHandler until startup so that Collections object is available
+Meteor.startup(function () {
+
+  Template.files_group.events({
+    'change input.any': FS.EventHandlers.insertFiles(Collections.Files, {
+      metadata: function (fileObj) {
+        var groupId = Session.get('groupId');
+          return {
+              owner:{
+                id: Meteor.userId(),
+                name: Meteor.user().profile.name
+              },
+              groupID: groupId,
+              dropped: false,
+              privacy:"public"
+            };
+      },
+      after: function (error, fileObj) {
+        if (!error) {
+          alert('done');
+        }
+      }
+    }),
+    'keyup .filename': function () {
+      var ins = Template.instance();
+      if (ins) {
+        ins.filename.set($('.filename').val());
+      }
+    }
+  });
+
+});
+
+
+
+Template.files_group.helpers({
+  uploadedFiles: function() {
+    return Collections.Files.find({});
   }
 });
