9d7b124c79349bcf132b38e7339d3350633e0670
sunil111 <langhi.sun@gmail.com>
2016-06-01 22:11:10 +0530

    Update Remainder

--- client/template/Reminder/Reminder.js
+++ client/template/Reminder/Reminder.js
@@ -1,4 +1,176 @@
-Template.createReminder.onRendered(function() {
+Template.CreateTodo.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('tasks');
+	});
+});
+
+Template.CreateTodo.onRendered(function() {
+	var today = new Date(); 
+	
+	$(function() {
+	  $('input[name="datefilter"]').daterangepicker({
+	    "singleDatePicker": true,
+	    "autoApply": true,
+	    "linkedCalendars": false,
+	    "startDate": today,
+	   
+	    "minDate": today,
+	    "maxDate": "12/31/2016",
+	    "timePicker": true
+	  });
+
+	  $('input[name="datefilter"]').on('apply.daterangepicker', function(ev, picker) {
+	      $(this).val(picker.endDate.format('MM/DD/YYYY h:mm A'));
+	  });
+
+	  $('input[name="datefilter"]').on('cancel.daterangepicker', function(ev, picker) {
+	      $(this).val('');
+	  });
+
+	});
+});
+
+Template.CreateTodo.events({
+    "submit .new-task": function (event) {
+		// Prevent default browser form submit
+		event.preventDefault();
+		// Get value from form element
+		var text = event.target.text.value;
+		var desc = event.target.desc.value;
+		var date= event.target.datefilter.value;
+		//var inputDate = date.toLocaleString();
+		//console.log(inputDate);
+		var time = date.slice(10);
+		console.log(time);
+		Meteor.call("createReminder",text, desc, date, function(err,res){
+			if(!err){
+				console.log("callback recieved: "+res);
+			}
+		});
+		
+		// Insert a task into the collection
+		// Clear form
+		event.target.text.value = "";
+		event.target.desc.value = "";
+		event.target.datefilter.value = "";
+		}
+  });
+
+
+Template.Task.events({
+	"click .toggle-checked": function () {
+	 	// Set the checked property to the opposite of its current value
+		Meteor.call("setCheckedReminder",this._id, !this.checked);
+	},
+	"click .delete": function () {
+		//Tasks.remove(this._id);
+		Meteor.call("deleteReminder",this._id);
+	}
+});
+
+//-------------Todo-------------------
+Template.YourTodo.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('tasks');
+	});
+});
+
+Template.YourTodo.events({
+	"change .hide-completed input": function (event) {
+		Session.set("hideCompleted", event.target.checked);
+	}
+});
+
+Template.YourTodo.helpers({
+    tasks: function () {
+      	if (Session.get("hideCompleted")) {
+        	// If hide completed is checked, filter tasks
+        	return Tasks.find({checked: {$ne: true},"owner.id": Meteor.userId(), action:"todo"}, {sort: {createdAt: -1}});
+      	} 
+      	else {
+        	// Otherwise, return all of the tasks
+        	return Tasks.find({ "owner.id": Meteor.userId(), action:"todo" }, {sort: {createdAt: -1}});
+      	}
+    },
+    task: function () {
+      	if (Session.get("hideCompleted")) {
+        	// If hide completed is checked, filter tasks
+        	return Tasks.find({checked: {$ne: true},"owner.id": Meteor.userId(), action:"todo"}).count();
+      	} 
+      	else {
+        	// Otherwise, return all of the tasks
+        	return Tasks.find({"owner.id": Meteor.userId(), action:"todo"}).count();
+      	}
+    },
+    hideCompleted: function () {
+      	return Session.get("hideCompleted");
+    },
+    incompleteCount : function(){
+      	return Tasks.find({checked : {$ne: true}, "owner.id": Meteor.userId(), action:"todo"}).count();
+    }
+  });
+
+//--------- Group Task----------------
+Template.GroupTask.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('tasks');
+		self.subscribe('groups');
+	});
+});
+
+Template.GroupTask.events({
+	"change .hide-completed input": function (event) {
+		Session.set("hideCompleted", event.target.checked);
+	}
+});
+
+Template.GroupTask.helpers({
+    tasks: function () {
+    	var groupId = Session.get('groupId'); 
+      	if (Session.get("hideCompleted")) {
+        	// If hide completed is checked, filter tasks
+        	return Tasks.find({checked: {$ne: true}, groupID:groupId, action:"task" }, {sort: {createdAt: -1}});
+      	} 
+      	else {
+        	// Otherwise, return all of the tasks
+
+        	return Tasks.find({ groupID: groupId, action:"task" }, {sort: {createdAt: -1}});
+      	}
+    },
+    task: function () {
+    	var groupId = Session.get('groupId');
+      	if (Session.get("hideCompleted")) {
+        	// If hide completed is checked, filter tasks
+        	return Tasks.find({checked: {$ne: true}, groupID:groupId, action:"task"}).count();
+      	} 
+      	else {
+        	// Otherwise, return all of the tasks
+        	return Tasks.find({groupID:groupId, action:"task"}).count();
+      	}
+    },
+    hideCompleted: function () {
+      	return Session.get("hideCompleted");
+    },
+    incompleteCount : function(){
+    	var groupId = Session.get('groupId');
+      	return Tasks.find({checked : {$ne: true}, groupID:groupId, action:"task" }).count();
+    }
+  });
+
+//--------------Group create task--------------
+Template.CreateTask.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('tasks');
+		self.subscribe('groups');
+	});
+	Session.get('groupId');
+});
+
+Template.CreateTask.onRendered(function() {
 	var today = new Date(); 
 	
 	$(function() {
@@ -23,59 +195,45 @@ Template.createReminder.onRendered(function() {
 
 	});
 });
-	
-Template.createReminder.onCreated(function(){
-	var self= this;
-	this.autorun( function() {
-		self.subscribe('tasks');
-	});
-});
-  
-Template.createReminder.helpers({
-    tasks: function () {
-      	if (Session.get("hideCompleted")) {
-        	// If hide completed is checked, filter tasks
-        	return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
-      	} 
-      	else {
-        	// Otherwise, return all of the tasks
-        	return Tasks.find({}, {sort: {createdAt: -1}});
-      	}
-    },
-    hideCompleted: function () {
-      	return Session.get("hideCompleted");
-    },
-    incompleteCount : function(){
-      	return Tasks.find({checked : {$ne: true}}).count();
-    }
-  });
 
-Template.createReminder.events({
+Template.CreateTask.events({
     "submit .new-task": function (event) {
 		// Prevent default browser form submit
 		event.preventDefault();
 		// Get value from form element
+		var arr= [];
 		var text = event.target.text.value;
 		var desc = event.target.desc.value;
 		var date= event.target.datefilter.value;
-		Meteor.call("createReminder",text, desc, date ,function(err,res){
+		var assign = event.target.assign.value;
+		var groupID= Session.get('groupId');
+		Meteor.call("createTask",text, desc, date, assign, groupID, function(err,res){
 			if(!err){
 				console.log("callback recieved: "+res);
 			}
 		});
+		
 		// Insert a task into the collection
 		// Clear form
 		event.target.text.value = "";
 		event.target.desc.value = "";
 		event.target.datefilter.value = "";
-		},
-		"change .hide-completed input": function (event) {
-			Session.set("hideCompleted", event.target.checked);
 		}
   });
 
+Template.CreateTask.helpers({
+	member: function(){
+		var groupId = Session.get('groupId');
+		console.log(groupId);
+		var group = Groups.findOne({_id: groupId});
+        var member= group.members;
+        console.log(member);
+        return member;
+	}
+});
 
-Template.task.events({
+
+Template.Task1.events({
 	"click .toggle-checked": function () {
 	 	// Set the checked property to the opposite of its current value
 		Meteor.call("setCheckedReminder",this._id, !this.checked);
