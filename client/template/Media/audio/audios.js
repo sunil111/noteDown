c34fe3c20ed5528542f425cb051ed910a685bf7d
sunil111 <langhi.sun@gmail.com>
2016-06-01 22:07:58 +0530

    Update Media

--- client/template/Media/audio/audios.js
+++ client/template/Media/audio/audios.js
@@ -2,7 +2,6 @@ Template.audio.created = function(){
 	this.filename = new ReactiveVar('');
 };
 
-
 Template.audio.onCreated(function(){
 	var self= this;
 	this.autorun( function() {
@@ -10,24 +9,23 @@ Template.audio.onCreated(function(){
 	});
 });
 
-
 Meteor.startup(function() {
 	Template.audio.events({
 	'change input.audioFile' : FS.EventHandlers.insertFiles(Collections.Audios,{
 		metadata : function(fileobj){
-			var groupId = Session.get('groupId');
 			return {
-	          owner:{
-	            id: Meteor.userId(),
-	            name: Meteor.user().profile.name
-	          },
-	          groupID: groupId,
-	          dropped: false
+	          	owner:{
+	            	id: Meteor.userId(),
+	            	name: Meteor.user().profile.name
+	          	},
+	          	dropped: false,
+          		privacy:"private"
 	        };
 		},
 
 		after : function (error,fileobj){
 			if(!error){
+				alert('done');
 			}
 		}
 	}),
@@ -45,7 +43,61 @@ Meteor.startup(function() {
 
 Template.audio.helpers({
   uploadedAudios: function() {
-  	var groupId = Session.get('groupId');
-    return Collections.Audios.find({groupID: groupId});
+  	
+    return Collections.Audios.find({});
+  }
+});
+
+Template.audio_group.created = function(){
+	this.filename = new ReactiveVar('');
+};
+
+
+Template.audio_group.onCreated(function(){
+	var self= this;
+	this.autorun( function() {
+		self.subscribe('audios');
+	});
+});
+
+
+Meteor.startup(function() {
+	Template.audio_group.events({
+	'change input.audioFile' : FS.EventHandlers.insertFiles(Collections.Audios,{
+		metadata : function(fileobj){
+			var groupId = Session.get('groupId');
+	    	return {
+	          	owner:{
+	            	id: Meteor.userId(),
+	            	name: Meteor.user().profile.name
+	          	},
+	          	groupID: groupId,
+	          	dropped: false,
+          		privacy:"public"
+	        };
+		},
+
+		after : function (error,fileobj){
+			if(!error){
+				alert('done');
+			}
+		}
+	}),
+
+
+	'keyup .filename': function(){
+		var ins = Template.instance();
+		if(ins){
+			ins.filename.set($('.filename').val());
+			}
+		}
+	});
+});
+
+
+Template.audio_group.helpers({
+  uploadedAudios: function() {
+  	
+    return Collections.Audios.find({});
   }
 });
