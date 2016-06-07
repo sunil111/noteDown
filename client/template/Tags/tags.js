238cc4debbbbbf74b2a25db7fa29ee39d8213bf7
sunil111 <langhi.sun@gmail.com>
2016-06-07 23:07:36 +0530

    Upadted

--- client/template/Tags/tags.js
+++ client/template/Tags/tags.js
@@ -2,11 +2,13 @@ var uniqueNum =0;
 var uniquevar ='A';
 var arr = [];
 
+
+
 Template.Tags.events({
 	"submit .form": function(event,tag){		
 		event.preventDefault();
 		var tagsVar = event.target.tagsTxt.value;
-		$('<div class="btn-group" style="margin-top:20px;"><input type="button" class="btn-group " id="tag'+ uniqueNum + uniquevar +'" value="'+ tagsVar + '" style=" border-right:none; border-radius:0px; border: 3px solid #808080; height:34px" readonly disabled><button class="glyphicon glyphicon-remove btn btn-group btn-danger btn-xs" id="tag'+uniqueNum +'" style="border-radius:0px; height:33px;"></button>&nbsp;&nbsp;&nbsp;&nbsp;</input></div>').insertAfter("h3");
+		$('<div class="btn-group" style="margin-top:20px;"><input type="button" class="btn-group btn-xs " id="tag'+ uniqueNum + uniquevar +'" value="'+ tagsVar + '" style=" border-right:none; border-radius:0px; border: 3px solid #808080; height:34px" readonly disabled><button class="glyphicon glyphicon-remove btn btn-group btn-danger btn-xs" id="tag'+uniqueNum +'" style="border-radius:0px; height:33px;"></button>&nbsp;&nbsp;&nbsp;&nbsp;</input></div>').insertAfter("h3");
     	var c = document.getElementById('tag'+ uniqueNum + uniquevar +'').value;
     	arr.push(c);
     	Session.set('tag', arr);
