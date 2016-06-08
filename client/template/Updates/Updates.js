0606afb56e0ccb6aeec65927e5263bb40c49d224
sunil111 <langhi.sun@gmail.com>
2016-06-08 14:25:31 +0530

    Updated

--- client/template/Updates/Updates.js
+++ client/template/Updates/Updates.js
@@ -4,31 +4,27 @@ Template.Updates.onCreated(function(){
 		self.subscribe('rss');
 		self.subscribe('user');
 	});
+	var rss= Rss.find({},{fields: { _id: 0, group_name: 1}});
+		for(var i=0; i< rss.length; i++){
+			var group_name= rss[i].group_name;
+			console.log(group_name);
+		}
 });
 
 Template.Updates.helpers({
 	feed:function(){
-		var feed= Rss.find({},
-			{sort: {createdAt: -1}},{limit: 6});
+		var feed= Rss.find({ user: Meteor.user().profile.name },{sort: {createdAt: -1}});
 		return feed;
 	},
 	feedCount:function(){
-		return Rss.find().count();
+		return Rss.find({}).count();
+	},
+	group: function(){
+		var rss= Rss.find({ });
+		console.log(rss.count());
+		for(var i=0; i< rss.length; i++){
+			var group_name= rss[i].group_name;
+			console.log(group_name);
+		}
 	}
-});
-
-Template.Updates.events({
-	/*'click #check': function(event){
-		var id= this._id;
-		var action= this.action;
-		var itemId= this.id;
-		console.log(itemId);
-		if(action=== "Post"){
-			Router.go('/group_notes/'+itemId);
-		}
-		else if(action=== "Group"){
-			Router.go('/group/'+itemId);
-		}
-		
-	}*/
 });
