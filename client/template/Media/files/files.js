d905071db69827d0e0bf1f48d41e665eb97806af
sunil111 <langhi.sun@gmail.com>
2016-06-08 14:18:03 +0530

    Updated

--- client/template/Media/files/files.js
+++ client/template/Media/files/files.js
@@ -71,12 +71,13 @@ Meteor.startup(function () {
         var group_name = group.gname;
         Rss.insert({
           rss_title: "has added a new file",
-          title: $('.filename').val(),
+          title: "file",
           user_action: "/user_dashboard/"+ Meteor.userId(),
           user_name: Meteor.user().profile.name,
           group_name: group_name,
           createdAt: new Date().toLocaleString(),
-          action: "/group/"+groupId
+          group_action: "/group/"+groupId,
+          action: '/group/'+groupId+'/shared_media/'
         });
           return {
               owner:{
@@ -90,7 +91,8 @@ Meteor.startup(function () {
       },
       after: function (error, fileObj) {
         if (!error) {
-          Router.go('/shared_media/');
+          var groupID = Session.get('groupId');
+          Router.go('/group/'+groupID+'/shared_media/');
         }
       }
     }),
