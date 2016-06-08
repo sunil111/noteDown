e9d51d00af0a6d5810bb110d3bc9018ad000b915
sunil111 <langhi.sun@gmail.com>
2016-06-08 14:15:51 +0530

    Updated

--- client/template/GetLocation/getLocation.js
+++ client/template/GetLocation/getLocation.js
@@ -68,22 +68,22 @@ if (navigator.geolocation){
         }, showError);
     }, 5000);
 } else {
-    console.log("Geolocation is not supported by this browser.");
+    //console.log("Geolocation is not supported by this browser.");
 }
 
 function showError(error) {
     switch(error.code) {
         case error.PERMISSION_DENIED:
-            console.log("User denied the request for Geolocation.");
+            //console.log("User denied the request for Geolocation.");
             break;
         case error.POSITION_UNAVAILABLE:
-            console.log("Location information is unavailable.");
+            //console.log("Location information is unavailable.");
           break;
         case error.TIMEOUT:
-            console.log("The request to get user location timed out.");
+            //console.log("The request to get user location timed out.");
             break;
         case error.UNKNOWN_ERROR:
-            console.log("An unknown error occurred.");
+            //console.log("An unknown error occurred.");
             break;
     }
 }
