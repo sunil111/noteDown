91c7c34c323e84005352944fa88ef2901d3a566e
sunil111 <langhi.sun@gmail.com>
2016-06-14 15:24:04 +0530

    Change css

--- client/template/Tags/tags.js
+++ client/template/Tags/tags.js
@@ -6,7 +6,7 @@ Template.Tags.events({
 	"submit .form": function(event,tag){		
 		event.preventDefault();
 		var tagsVar = event.target.tagsTxt.value;
-		$('<div class="btn-group" style="margin-top:20px;"><input type="button" class="btn-group btn-xs " id="tag'+ uniqueNum + uniquevar +'" value="'+ tagsVar + '" style=" border-right:none; border-radius:0px; border: 3px solid #808080; height:34px" readonly disabled><button class="glyphicon glyphicon-remove btn btn-group btn-danger btn-xs" id="tag'+uniqueNum +'" style="border-radius:0px; height:33px;"></button>&nbsp;&nbsp;&nbsp;&nbsp;</input></div>').insertAfter("h3");
+		$('<div class="btn-group" style="margin-top:10px; margin-bottom:4px margin-left:1px margin-right:1px background:#9a9aff"><input type="button" class="btn-group btn-xs " id="tag'+ uniqueNum + uniquevar +'" value="'+ tagsVar + '" style=" background:#eafaff; border-right:none; border-radius:0px; border: 1px solid border: 1px solid #000000; height:34px" readonly disabled><button class="glyphicon glyphicon-remove btn btn-group btn-danger btn-md" id="tag'+uniqueNum +'" style="border-radius:0px; height:33px; border: 3px solid #808080"></button>&nbsp;&nbsp;&nbsp;&nbsp;</input></div>').insertAfter("h3");
     	var c = document.getElementById('tag'+ uniqueNum + uniquevar +'').value;
     	arr.push(c);
     	Session.set('tag', arr);
